808a72e80d37d979f2db0e6f92cf6ea87f29dc9c
Kamil <>
2023-05-08 14:17:18 +0200

    Android app initial commit

---
+++ application/app/src/main/java/app/autko/MainActivity.java
@@ -0,0 +1,36 @@
+package app.autko;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final SeekBar seekBar = findViewById(R.id.slider);
+        final TextView textView = findViewById(R.id.label);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(Integer.toString(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+
+}
