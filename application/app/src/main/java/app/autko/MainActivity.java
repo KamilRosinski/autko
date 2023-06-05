cdc545191ad1fd9230bef2eaf5acd3897c83a7fe
Kamil <>
2023-06-05 14:03:32 +0200

    [app] Ability to turn Bluetooth on

--- application/app/src/main/java/app/autko/MainActivity.java
+++ application/app/src/main/java/app/autko/MainActivity.java
@@ -1,8 +1,22 @@
 package app.autko;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -15,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
     private MainActivityViewModel viewModel;
 
+    private ActivityResultLauncher<String> requestPermissionLauncher;
+
+    private final Intent btEnableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +44,38 @@ public class MainActivity extends AppCompatActivity {
         binding.setViewModel(viewModel);
 
         viewModel.setProgress(0);
+
+        final BluetoothManager btManager = getSystemService(BluetoothManager.class);
+        final BluetoothAdapter btAdapter = btManager.getAdapter();
+        viewModel.setBtSupported(btAdapter != null);
+        viewModel.setBtState(btAdapter != null ? btAdapter.getState() : BluetoothAdapter.ERROR);
+
+        final IntentFilter intentFilter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        final BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                viewModel.setBtState(intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR));
+            }
+        };
+        registerReceiver(receiver, intentFilter);
+
+        requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), granted -> {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
+                Log.d("BT PERMISSION", "Permission granted");
+                startActivity(btEnableIntent);
+            } else {
+               Log.d("BT PERMISSION", "Permission rejected");
+            }
+        });
+    }
+
+    public void enableBt(final View view) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
+            startActivity(btEnableIntent);
+        } else {
+            Log.d("BT PERMISSION", "Request permission");
+            requestPermissionLauncher.launch(Manifest.permission.BLUETOOTH_CONNECT);
+        }
     }
 
 }
--- application/app/src/main/java/app/autko/viewmodel/MainActivityViewModel.java
+++ application/app/src/main/java/app/autko/viewmodel/MainActivityViewModel.java
@@ -1,12 +1,19 @@
 package app.autko.viewmodel;
 
+import android.bluetooth.BluetoothAdapter;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
+
+import app.autko.util.BtLabelProvider;
 
 public class MainActivityViewModel extends ViewModel {
 
     private final MutableLiveData<Integer> progress = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> btSupported = new MutableLiveData<>();
+    private final MutableLiveData<Integer> btState = new MutableLiveData<>();
 
     public LiveData<Integer> getProgress() {
         return progress;
@@ -14,6 +21,30 @@ public class MainActivityViewModel extends ViewModel {
 
     public void setProgress(final Integer progress) {
         this.progress.postValue(progress);
+    }
+
+    public LiveData<Boolean> isBtSupported() {
+        return btSupported;
+    }
+
+    public void setBtSupported(final Boolean btSupported) {
+        this.btSupported.postValue(btSupported);
+    }
+
+    public LiveData<Integer> getBtState() {
+        return btState;
+    }
+
+    public void setBtState(final int btState) {
+        this.btState.postValue(btState);
+    }
+
+    public LiveData<String> getBtStateLabel() {
+        return Transformations.map(btState, BtLabelProvider::getLabel);
+    }
+
+    public LiveData<Boolean> isBtDisabled() {
+        return Transformations.map(btState, btState -> btState == BluetoothAdapter.STATE_OFF);
     }
 
 }
